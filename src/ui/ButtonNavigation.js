1f3cc4bf786d6191064f695dc8b53a5289db0e0b
Alexandr Guzenko <35737301+AlexandrGuzenko@users.noreply.github.com>
2018-08-28 20:56:57 +0300

    Add main code files

---
+++ src/ui/ButtonNavigation.js
@@ -0,0 +1,77 @@
+import {
+  ButtonToolbar,
+  ButtonGroup,
+  Button,
+} from 'react-bootstrap';
+import React from 'react';
+import styles from '../css/buttons.css';
+
+const ButtonNavigation = ({ curPage, onChange, maxPage }) => {
+  const buttonPanel = [];
+
+  if (maxPage < 7) {
+    for (let i = 0; i < maxPage; i++) {
+      buttonPanel.push(
+        <Button
+          bsSize="large"
+          bsStyle={(curPage === i + 1) ? 'success' : 'default'}
+          onClick={() => onChange(i + 1)}
+          key={i}
+        >
+          {i + 1}
+        </Button>,
+      );
+    }
+  } else if (curPage < 4) {
+    for (let i = 0; i < 7; i++) {
+      buttonPanel.push(
+        <Button
+          bsSize="large"
+          bsStyle={(curPage === i + 1) ? 'success' : 'default'}
+          onClick={() => onChange(i + 1)}
+          key={i}
+        >
+          {i + 1}
+        </Button>,
+      );
+    }
+  } else if (curPage < maxPage - 3) {
+    for (let i = -3; i < 4; i++) {
+      buttonPanel.push(
+        <Button
+          bsSize="large"
+          bsStyle={(curPage === curPage + i) ? 'success' : 'default'}
+          onClick={() => onChange(curPage + i)}
+          key={i}
+        >
+          {curPage + i}
+        </Button>,
+      );
+    }
+  } else {
+    for (let i = maxPage - 6; i <= maxPage; i++) {
+      buttonPanel.push(
+        <Button
+          bsSize="large"
+          bsStyle={(curPage === i) ? 'success' : 'default'}
+          onClick={() => onChange(i)}
+          key={i}
+        >
+          {i}
+        </Button>,
+      );
+    }
+  }
+
+  return (
+    <div className={(buttonPanel.length < 5) ? styles.smallWrapper : styles.wrapper}>
+      <ButtonToolbar>
+        <ButtonGroup>
+          {buttonPanel}
+        </ButtonGroup>
+      </ButtonToolbar>
+    </div>
+  );
+};
+
+export default ButtonNavigation;
---
+++ src/ui/ListRow.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  PanelGroup,
+  Panel,
+} from 'react-bootstrap';
+import styles from '../css/App.css';
+
+const ListRow = ({
+  number, title, year, id, rating, page, overview,
+}) => (
+  <tr>
+    <td>
+      {(page - 1) * 10 + number + 1}
+    </td>
+    <td>
+      <img src={id} alt="something wrong..." />
+      {' '}
+    </td>
+    <td>
+
+      <PanelGroup accordion id="accordion-example">
+        <Panel eventKey="1">
+          <Panel.Heading>
+            <Panel.Title toggle>
+              <h3>
+                {title}
+              </h3>
+            </Panel.Title>
+          </Panel.Heading>
+          <Panel.Body collapsible>
+            <p className={styles.overview}>
+              {overview}
+            </p>
+          </Panel.Body>
+        </Panel>
+      </PanelGroup>
+    </td>
+    <td>
+      {year}
+    </td>
+    <td>
+      {Math.round(rating * 100) / 100}
+    </td>
+  </tr>
+);
+
+export default ListRow;
